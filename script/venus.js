9aef86b24294edbbf98469a8d12deef27488ebd5
Gabriel Rodrigues <gabriel.rodriguesdds@gmail.com>
2024-05-14 16:00:29 -0300

    feat: script and html venus

--- script/venus.js
+++ script/venus.js
@@ -16,10 +16,7 @@ const VenusInternalStructure = (event) =>{
     event.preventDefault()
     
     const textPlanets = document.getElementById("texto-planetas")
-    textPlanets.innerText = `The similarity in size and density between Venus and Earth suggests 
-    they share a similar internal structure: a core, mantle, and crust. 
-    Like that of Earth, Venusian core is most likely at least partially 
-    liquid because the two planets have been cooling at about the same rate.
+    textPlanets.innerText = `The similarity in size and density between Venus and Earth suggests they share a similar internal structure: a core, mantle, and crust. Like that of Earth, Venusian core is most likely at least partially liquid because the two planets have been cooling at about the same rate.
   `
 
     document.getElementById("source-planetas").setAttribute("href", "https://en.wikipedia.org/wiki/Venus#Internal_structure")
@@ -35,10 +32,7 @@ const VenusSurfaceGeology = (event) => {
     event.preventDefault()
     
     const textPlanets = document.getElementById("texto-planetas")
-    textPlanets.innerText = `Much of the Venusian surface appears to have been shaped by volcanic activity. 
-    Venus has several times as many volcanoes as Earth, and it has 167 large 
-    volcanoes that are over 100 km (60 mi) across. The only volcanic complex 
-    of this size on Earth is the Big Island of Hawaii.`
+    textPlanets.innerText = `Much of the Venusian surface appears to have been shaped by volcanic activity. Venus has several times as many volcanoes as Earth, and it has 167 large volcanoes that are over 100 km (60 mi)across. The only volcanic complex of this size on Earth is the Big Island of Hawaii.`
     
     document.getElementById("source-planetas").setAttribute("href", "https://en.wikipedia.org/wiki/Venus#Surface_geology")
     
