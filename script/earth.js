a89646162681caebe2191c66b7eff0cbaf72c173
Gabriel Rodrigues <gabriel.rodriguesdds@gmail.com>
2024-05-14 15:41:47 -0300

    feat: add venus.js and venus.html

--- script/earth.js
+++ script/earth.js
@@ -4,7 +4,7 @@ const EarthOverview = (event) => {
     const textPlanets = document.getElementById('texto-planetas')
     textPlanets.innerText = `Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is land with remaining 70.8% is covered with water. Earth's distance from the Sun, physical properties and geological history have allowed life to evolve and thrive.`
 
-    document.getElementById("source-planetas").setAttribute("href", "https://en.wikipedia.org/wiki/Earth")
+    document.getElementById("source-planetas").setAttribute("href", "https://en.wikipedia.org/wiki/Earth#Internal_structure")
     
     document.getElementById('img-planetas').src = "assets/planet-earth.svg"
 
